f0aa3b76d03170fe74adf4aa1b86c43cce8e7b15
Kylee <kkeith.biz@gmail.com>
2025-09-24 14:24:01 -0400

    Finished Calculator

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -6,10 +6,10 @@ public class Calculator {
     public static void main(String[] args) {
        Scanner scan = new Scanner(System.in);
 
-       System.out.print("Enter the first number:")
+       System.out.print("Enter the first number:");
                int num1 = scan.nextInt();
 
-        System.out.print("Enter the second number:")
+        System.out.print("Enter the second number:");
         int num2 = scan.nextInt();
 
         System.out.println("Now we will calculate you number");
