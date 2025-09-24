0575d3c73832e2d4f64a3ae747d029c2476e140b
Kylee <kkeith.biz@gmail.com>
2025-09-24 14:20:03 -0400

    First num

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+
+       System.out.print("Enter the first number:")
+               int num1 = scan.nextInt();
+
+    }
 }
